cc5a60f3605d8a1c4e5cc6fea84c589117075f23
José Francisco Ruiz Zamora <jrz899@inlumine.ual.es>
2020-06-06 01:43:35 +0200

    added cors

--- app.js
+++ app.js
@@ -16,12 +16,19 @@ app.use("/",apiRouter)
 
 app.use(cors());
 app.options('*', cors());
-app.use(function(req,res,next){
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+    });
+/*app.use(function(req,res,next){
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType, Content-Type, Accept, Authorization");
     next();
-});
+});*/
 /*app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
