3d425c387368f726ad3562e569422c86226b35c2
José Francisco Ruiz Zamora <jrz899@inlumine.ual.es>
2020-06-05 20:20:14 +0200

    modified cors

--- app.js
+++ app.js
@@ -15,12 +15,20 @@ const app = express();
 app.use("/",apiRouter)
 
 app.use(cors());
-app.use(function(req,res,next){
+/*app.use(function(req,res,next){
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,PUT,POST");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType, Content-Type, Accept, Authorization");
     next();
-})
+});*/
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 var swaggerUi = require('swagger-ui-express'),
     swaggerDocument = require('./swagger.json');
